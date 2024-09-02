cbd987b27c295f8903ed3e1c4899211aa426f2a0
Jungwoo LEE <jungwoo3490@naver.com>
2024-09-02 21:15:32 +0900

    chore(packages/ui): vitest 초기세팅을 진행합니다. (#12)

---
+++ packages/ui/src/Button.test.tsx
@@ -0,0 +1,11 @@
+
+import { render, screen } from '@testing-library/react';
+import { Button } from './button';
+
+it("버튼 컴포넌트가 정상적으로 렌더링됩니다.", () => {
+    render(<Button children="test" className='test' appName='test'/>);
+
+    expect(
+        screen.getByRole('button', { name: /test/i }),
+      ).toBeInTheDocument();
+})
---
+++ packages/ui/vitest.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude: ['**/node_modules/**', '**/dist/**'],
+  },
+  resolve: {
+    alias: {
+      '@repo': './packages',
+    },
+  },
+});
