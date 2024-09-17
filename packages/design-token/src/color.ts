d0fd22c2f23c5482229d70980dab0e858255605b
Jungwoo LEE <jungwoo3490@naver.com>
2024-09-17 21:18:52 +0900

    feat(design-token): design-token 패키지를 구축합니다. (#21)

---
+++ packages/design-token/src/color.ts
@@ -0,0 +1,25 @@
+const color = {
+  key: "#3875EC",
+  white: "#FFFFFF",
+  black: "#000000",
+
+  sub: {
+    1: "#ECF4FD",
+    2: "#DEEDFF",
+    3: "#BCDBFF",
+    4: "#8EC2FF",
+  },
+
+  gray: {
+    1: "#F8F8F8",
+    1.3: "#E7E7E7",
+    1.5: "#D4E1E7",
+    2: "#DFE3E5",
+    3: "#B3BAC0",
+    4: "#979AA0",
+    5: "#454545",
+    6: "#191919",
+  },
+};
+
+export default color;
