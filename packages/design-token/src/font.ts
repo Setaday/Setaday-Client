005180cc0f8db9a6938b41ea7f6e6745c83ac57c
Jungwoo LEE <jungwoo3490@naver.com>
2024-09-20 06:14:26 +0900

    fix(design-token): design token의 변경사항을 반영합니다. (#27)

--- packages/design-token/src/font.ts
+++ packages/design-token/src/font.ts
@@ -7,13 +7,13 @@ const font = {
   'body5_m_14': ['1.4rem', { lineHeight: 'auto', letterSpacing: '0px', fontWeight: '500', fontFamily: 'DM Sans'}],
   'body6_m_12': ['1.2rem', { lineHeight: 'auto', letterSpacing: '0px', fontWeight: '500', fontFamily: 'DM Sans'}],
   'button2_sb_20': ['2rem', { lineHeight: '140%', letterSpacing: '-0.5px', fontWeight: '600', fontFamily: 'Pretendard'}],
-  'head2_sb_18': ['1.8rem', { lineHeight: '140%', letterSpacing: '-1px', fontWeight: '600', fontFamily: 'Pretendard'}],
+  'head2_sb_18': ['1.8rem', { lineHeight: '140%', letterSpacing: '-0.18px', fontWeight: '600', fontFamily: 'Pretendard'}],
   'button1_sb_18': ['1.8rem', { lineHeight: '140%', letterSpacing: '-0.5px', fontWeight: '600', fontFamily: 'Pretendard'}],
-  'title1_sb_16': ['1.6rem', { lineHeight: '140%', letterSpacing: '-0.2px', fontWeight: '600', fontFamily: 'Pretendard'}],
-  'body7_m_16': ['1.6rem', { lineHeight: '140%', letterSpacing: '-1px', fontWeight: '500', fontFamily: 'Pretendard'}],
-  'caption1_m_12': ['1.2rem', { lineHeight: 'auto', letterSpacing: '-0px', fontWeight: '500', fontFamily: 'Pretendard'}],
-  'body8_m_14': ['1.4rem', { lineHeight: 'auto', letterSpacing: '-1.5px', fontWeight: '500', fontFamily: 'Pretendard'}],
-  'body9_sb_14': ['1.4rem', { lineHeight: 'auto', letterSpacing: '-2px', fontWeight: '600', fontFamily: 'Pretendard'}],
+  'title1_sb_16': ['1.6rem', { lineHeight: '140%', letterSpacing: '-0.032px', fontWeight: '600', fontFamily: 'Pretendard'}],
+  'body7_m_16': ['1.6rem', { lineHeight: '140%', letterSpacing: '-0.16px', fontWeight: '500', fontFamily: 'Pretendard'}],
+  'caption1_m_12': ['1.2rem', { lineHeight: 'auto', letterSpacing: '0px', fontWeight: '500', fontFamily: 'Pretendard'}],
+  'body8_m_14': ['1.4rem', { lineHeight: 'auto', letterSpacing: '-0.21px', fontWeight: '500', fontFamily: 'Pretendard'}],
+  'body9_sb_14': ['1.4rem', { lineHeight: 'auto', letterSpacing: '-0.28px', fontWeight: '600', fontFamily: 'Pretendard'}],
 };
 
 export default font;
