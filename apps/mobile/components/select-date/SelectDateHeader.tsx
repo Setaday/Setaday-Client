fef3fed12c2251f0d63b3d149d16e598f6eee6af
seoAreum <1971236@hansung.ac.kr>
2024-11-12 16:31:07 +0900

    feat: next/navigation을 활용한 코드로 변경

--- apps/mobile/components/select-date/SelectDateHeader.tsx
+++ apps/mobile/components/select-date/SelectDateHeader.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import { MobileIconArrowLeftBlack } from "@setaday/icon";
+import { useRouter } from "next/navigation";
 
 function SelectDateHeader() {
+  const router = useRouter();
+  const clickArrowBtn = () => {
+    router.back();
+  };
   return (
     <header className="border-gray-1 flex h-[5.2rem] w-[100dvw] items-center justify-between border-b-[1px] px-[0.9rem]">
-      <button onClick={() => window.history.back()}>
+      <button onClick={clickArrowBtn}>
         <MobileIconArrowLeftBlack />
       </button>
 
