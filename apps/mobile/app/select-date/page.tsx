e6b6d41cf5ebd71e884a601172ddde2ea0790cb2
seoAreum <1971236@hansung.ac.kr>
2024-11-26 19:19:00 +0900

    feat(mobile): 시간선택 뷰 input 에러상태 정의

--- apps/mobile/app/select-date/page.tsx
+++ apps/mobile/app/select-date/page.tsx
@@ -5,22 +5,27 @@ import { useState } from "react";
 
 export default function page() {
   const PLACE_HOLDER = "약속 이름을 작성해주세요";
+  const MAX_LENGTH = 16;
 
   const [planName, setPlanName] = useState("");
+  const [isInputError, setIsInputError] = useState(false);
 
   const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.currentTarget;
     setPlanName(value);
+
+    value.length > MAX_LENGTH ? setIsInputError(true) : setIsInputError(false);
   };
 
   return (
     <>
       <TextField
+        inputSize="mobile"
         placeholder={PLACE_HOLDER}
         value={planName}
+        isError={isInputError}
+        maxLength={MAX_LENGTH}
         onChange={handleChangeInput}
-        maxLength={16}
-        inputSize="mobile"
       />
     </>
   );
