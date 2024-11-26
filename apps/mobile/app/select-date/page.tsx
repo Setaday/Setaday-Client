6656a8a1f0a076c5a19fecbb7e20387dedc275bb
seoAreum <1971236@hansung.ac.kr>
2024-11-26 19:14:07 +0900

    feat(mobile): 시간선택 뷰 약속 이름을 정하는 input 구현

--- apps/mobile/app/select-date/page.tsx
+++ apps/mobile/app/select-date/page.tsx
@@ -1,3 +1,27 @@
+"use client";
+
+import { TextField } from "../../../../packages/ui/src/TextField";
+import { useState } from "react";
+
 export default function page() {
-  return <div>hi</div>;
+  const PLACE_HOLDER = "약속 이름을 작성해주세요";
+
+  const [planName, setPlanName] = useState("");
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.currentTarget;
+    setPlanName(value);
+  };
+
+  return (
+    <>
+      <TextField
+        placeholder={PLACE_HOLDER}
+        value={planName}
+        onChange={handleChangeInput}
+        maxLength={16}
+        inputSize="mobile"
+      />
+    </>
+  );
 }
