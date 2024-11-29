84c741342a66448541bf9c210e72c3aa1865037d
seoAreum <1971236@hansung.ac.kr>
2024-11-29 16:55:44 +0900

    feat(mobile): 캘린더 퍼블리싱

--- apps/mobile/components/select-date/SelectDateCalendar.tsx
+++ apps/mobile/components/select-date/SelectDateCalendar.tsx
@@ -4,6 +4,10 @@ import { MobileIconArrowLeftGray, MobileIconArrowRightGray } from "@setaday/icon
 import { useState } from "react";
 
 function SelectDateCalendar() {
+  const DAY = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
+  const DATE = Array.from({ length: 30 }, (_, idx) => idx + 1);
+  const REMAIN_DATE = Array.from({ length: 35 - DATE.length }, (_, idx) => 31 - 5 + (idx + 1));
+
   const [month, setMonth] = useState(new Date().getMonth() + 1);
 
   const changeMonthToEng = (month: number) => {
@@ -46,8 +50,8 @@ function SelectDateCalendar() {
   };
 
   return (
-    <article>
-      <header className="flex items-center">
+    <article className="flex flex-col">
+      <header className="flex items-center justify-center mb-[2.2rem]">
         <button type="button" onClick={() => handleClickArrow("left")}>
           <MobileIconArrowLeftGray />
         </button>
@@ -56,6 +60,35 @@ function SelectDateCalendar() {
           <MobileIconArrowRightGray />
         </button>
       </header>
+
+      <article className="flex flex-col gap-[3.1rem]">
+        <header className="grid grid-cols-7 gap-x-[2.5rem]">
+          {DAY.map((day) => {
+            return (
+              <h2 key={day} className="w-[2.9rem] font-body6_m_12 text-gray-2 text-center">
+                {day}
+              </h2>
+            );
+          })}
+        </header>
+
+        <div className="grid grid-cols-7 grid-rows-5 gap-x-[2.5rem] gap-y-[3.7rem]">
+          {REMAIN_DATE.map((reminder) => {
+            return (
+              <p key={reminder} className="w-[2.9rem] h-[1.8rem] font-body5_m_14 text-gray-4 text-center">
+                {reminder}
+              </p>
+            );
+          })}
+          {DATE.map((date) => {
+            return (
+              <p key={date} className="w-[2.9rem] h-[1.8rem] font-body5_m_14 text-gray-6 text-center">
+                {date}
+              </p>
+            );
+          })}
+        </div>
+      </article>
     </article>
   );
 }
