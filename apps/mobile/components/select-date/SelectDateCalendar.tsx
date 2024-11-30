3b68315afb3bb95b515a6beaae2d04f727d8e0b3
seoAreum <1971236@hansung.ac.kr>
2024-11-30 15:56:09 +0900

    feat(mobile): 날짜 선택 로직 구현

--- apps/mobile/components/select-date/SelectDateCalendar.tsx
+++ apps/mobile/components/select-date/SelectDateCalendar.tsx
@@ -7,7 +7,14 @@ import { getCalendarDate } from "../../constants/getCaledarDate";
 function SelectDateCalendar() {
   const [year, setYear] = useState(new Date().getFullYear());
   const [month, setMonth] = useState(new Date().getMonth() + 1);
-  const [selectedDate, setSelectedDate] = useState<Array<number>>([]);
+  const [selectedDate, setSelectedDate] = useState<
+    Array<{
+      startMonth: number;
+      startDate: number;
+      endMonth: number;
+      endDate: number;
+    }>
+  >([{ startMonth: 0, startDate: 0, endMonth: 0, endDate: 0 }]);
 
   const DAY = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
   const ALL_DATE = getCalendarDate({ year, month });
@@ -43,7 +50,6 @@ function SelectDateCalendar() {
 
   const handleClickArrow = (arrow: string) => {
     const isLeft = arrow === "left";
-    setSelectedDate([]);
 
     if (isLeft) {
       if (month === 1) {
@@ -62,12 +68,55 @@ function SelectDateCalendar() {
     }
   };
 
-  const handleClickDate = (date: number) => {
-    if (selectedDate.includes(date)) {
-      const newDate = selectedDate.filter((includedDate) => includedDate !== date);
+  const handleClickDate = ({
+    clickedMonth,
+    date,
+  }: {
+    clickedMonth: number;
+    date: number;
+  }) => {
+    const idxOfDate = selectedDate.findIndex(
+      (savedDate) =>
+        (savedDate.startDate === date && savedDate.startMonth === clickedMonth) ||
+        (savedDate.endDate === date && savedDate.endMonth === clickedMonth)
+    );
+
+    // 이미 선택된 날짜인 경우, -1보다 큰 idx 값 반환
+    if (idxOfDate > -1) {
+      const newDate = selectedDate.map((savedDate) => {
+        // startDate가 date와 같고 startMonth가 clickedMonth와 같으면 startDate와 startMonth만 초기화
+        if (savedDate.startDate === date && savedDate.startMonth === clickedMonth) {
+          return { ...savedDate, startDate: 0, startMonth: 0 };
+        }
+
+        // endDate가 date와 같고 endMonth가 clickedMonth와 같으면 endDate와 endMonth만 초기화
+        if (savedDate.endDate === date && savedDate.endMonth === clickedMonth) {
+          return { ...savedDate, endDate: 0, endMonth: 0 };
+        }
+
+        // 조건에 맞지 않으면 그대로 반환
+        return savedDate;
+      });
       setSelectedDate([...newDate]);
     } else {
-      setSelectedDate([...selectedDate, date]);
+      const lastDate = selectedDate[selectedDate.length - 1];
+
+      if ((lastDate && lastDate.startDate === 0 && lastDate.endDate === 0) || selectedDate.length === 0) {
+        setSelectedDate([{ startMonth: month, startDate: date, endMonth: 0, endDate: 0 }]);
+      } else if (lastDate && lastDate.startDate !== 0 && lastDate.endDate === 0) {
+        setSelectedDate((prev) => [
+          // 이전 값을 유지한 채 마지막 요소만 변경
+          ...prev.slice(0, -1),
+          {
+            startMonth: lastDate.startMonth,
+            startDate: lastDate.startDate,
+            endMonth: clickedMonth,
+            endDate: date,
+          },
+        ]);
+      } else if (selectedDate.length > 0) {
+        setSelectedDate((prev) => [...prev, { startMonth: month, startDate: date, endMonth: 0, endDate: 0 }]);
+      }
     }
   };
 
@@ -98,7 +147,13 @@ function SelectDateCalendar() {
           {ALL_DATE.map(({ id, date, color }) =>
             date.map((num) => {
               const isActiveClick = id === "currentDate";
-              const isClickedNum = isActiveClick && selectedDate.includes(num);
+              const idxOfDate = selectedDate.findIndex(
+                (savedDate) =>
+                  (savedDate.startDate === num && savedDate.startMonth === month) ||
+                  (savedDate.endDate === num && savedDate.endMonth === month)
+              );
+
+              const isClickedNum = isActiveClick && idxOfDate > -1;
 
               return (
                 // biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
@@ -107,7 +162,7 @@ function SelectDateCalendar() {
                   className={`flex items-center justify-center px-[1.35rem] py-[1.1rem] rounded-full font-body5_m_14  ${color} ${
                     isClickedNum && "bg-key"
                   } `}
-                  onClick={() => isActiveClick && handleClickDate(num)}
+                  onClick={() => isActiveClick && handleClickDate({ clickedMonth: month, date: num })}
                 >
                   {num}
                 </p>
