e3e1e14fab75d3bede4a1e5c147dc3317fb44272
Jungwoo LEE <jungwoo3490@naver.com>
2024-12-08 00:11:26 +0900

    feat(ui): TextField에 maxLength Error 처리 로직을 추가합니다. (#96)

--- packages/ui/src/TextField/TextField.styles.ts
+++ packages/ui/src/TextField/TextField.styles.ts
@@ -1,7 +1,7 @@
 import { cva } from "class-variance-authority";
 
 export const textFieldVariants = cva(
-  `rounded-[0.8rem] bg-gray-1 flex justify-between items-center px-[2rem] gap-[1rem]`,
+  "rounded-[0.8rem] mb-[0.4rem] bg-gray-1 flex justify-between items-center px-[2rem] gap-[1rem]",
   {
     variants: {
       isError: {
@@ -10,8 +10,16 @@ export const textFieldVariants = cva(
       inputSize: {
         desktop: "w-[33.5rem] h-[5.7rem]",
         desktop_lg: "w-[51rem] h-[6.1rem]",
-        mobile: "w-[33.5rem] h-[4.8rem]",
+        mobile: "w-[100%] h-[4.8rem]",
       },
     },
-  },
+  }
 );
+
+export const textCountVariants = cva("", {
+  variants: {
+    isError: {
+      true: "text-red-100",
+    },
+  },
+});
