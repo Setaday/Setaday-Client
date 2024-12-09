c038ea9114f63b56214afa90133dbf2e925e3213
seoAreum <80264647+Arooming@users.noreply.github.com>
2024-12-09 14:07:12 +0900

    feat(mobile): 날짜 선택 뷰 캘린더를 구현합니다. (#104)

---
+++ apps/mobile/contants/calendarConst.ts
@@ -0,0 +1,16 @@
+export const DAY = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+export const MONTH_NAMES = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
